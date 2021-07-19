ae06a7c2b480ec9e3722e5b40ebf52d778fc048c
LLYS-Phys <lachyordanov@gmail.com>
2021-07-20 00:54:42 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,46 +1,124 @@
-var a = '';
-var b = '';
-var num = [];
-var ans;
+const calc = {
+	displayValue: '0',
+	firstOperand: null,
+	waitingForSecondOperand: false,
+	operator: null,
+};
 
-function sendNum(digit){
-	num.push(digit);
-	if(num.length != 1){
-		a = '';
-		document.getElementById('screen').innerHTML = a;
+function inputDigit(digit) {
+	const { displayValue, waitingForSecondOperand } = calc;
+	if (waitingForSecondOperand === true) {
+	  calc.displayValue = digit;
+	  calc.waitingForSecondOperand = false;
+	} 
+	else {
+	  calc.displayValue = displayValue === '0' ? digit : displayValue + digit;
 	}
-	for(i=0; i<num.length ; i++){
-		a = a + num[i];
-        if (num.length > 14){
-            alert("You have reached the maximum amount of input numbers")
-            num.pop();
-        }
+	if (displayValue.length>11){
+		alert("Your numbers are too big")
 	}
-document.getElementById('screen').innerHTML = a;
+}
+  
+function inputDecimal(dot) {
+	if (calc.waitingForSecondOperand === true) {
+		calc.displayValue = '0.'
+	  	calc.waitingForSecondOperand = false;
+	  	return
+	}
+	if (!calc.displayValue.includes(dot)) {
+	  	calc.displayValue += dot;
+	}
+}
+  
+function handleOperator(nextOperator) {
+	const { firstOperand, displayValue, operator } = calc
+	const inputValue = parseFloat(displayValue);
+  
+	if (operator && calc.waitingForSecondOperand)  {
+		calc.operator = nextOperator;
+		return;
+	}
+
+	if (firstOperand == null && !isNaN(inputValue)) {
+	  	calc.firstOperand = inputValue;
+  
+	} 
+	else if (operator) {
+	  	const result = calculate(firstOperand, inputValue, operator);
+	  	calc.displayValue = `${parseFloat(result.toFixed(7))}`;
+	  	calc.firstOperand = result;
+	}
+	calc.waitingForSecondOperand = true;
+	calc.operator = nextOperator;
+	console.log(calc);
+}
+  
+function calculate(firstOperand, secondOperand, operator) {
+	if (operator === '+') {
+	  	return firstOperand + secondOperand;
+	} 
+	else if (operator === '-') {
+	  	return firstOperand - secondOperand;
+	} 
+	else if (operator === '*') {
+	  	return firstOperand * secondOperand;
+	} 
+	else if (operator === '/') {
+	  	return firstOperand / secondOperand;
+	}
+	return secondOperand;
+  }
+
+function resetCalc() {
+	calc.displayValue = '0';
+	calc.firstOperand = null;
+	calc.waitingForSecondOperand = false;
+	calc.operator = null;
 }
 
-function equalTo(){
-	document.getElementById('screen').innerHTML = '';
-	for(i=0; i<num.length ; i++){
-		b += num[i];
-	}
-	ans = eval(b);	
-	document.getElementById('screen').innerHTML = ans;
-	while(num.length > 0){
-    	num.pop();
-	}
-	num.push(ans.toString());
+function cEntry(){
+	calc.displayValue = '0';
+	calc.waitingForSecondOperand = true;
 }
-
-function clearAll(){
-	document.getElementById('screen').innerHTML = '';	
-	while(num.length > 0){
-    	num.pop();
+  
+function updateDisplay() {
+	const display = document.querySelector('.calc-screen');
+	display.value = calc.displayValue;
+}
+  
+updateDisplay();
+  
+const keys = document.querySelector('.keyboard');
+  
+keys.addEventListener('click', event => {
+	const { target } = event;
+	const { value } = target;
+	if (!target.matches('button')) {
+	  return;
 	}
-	a ='';	
-	b ='';
-}
-
-function clearEntry(){
-    
-}
+  
+	switch (value) {
+	  	case '+':
+	  	case '-':
+	  	case '*':
+	  	case '/':
+	  	case '=':
+			handleOperator(value);
+			break;
+	  	case '.':
+			inputDecimal(value);
+			break;
+		case 'clearEntry':
+			cEntry();
+			break
+	  	case 'clearAll':
+			resetCalc();
+			break;
+	  	default:
+			if (Number.isInteger(parseFloat(value))) {
+		  		inputDigit(value);
+			}
+	}
+  
+	updateDisplay();
+});
